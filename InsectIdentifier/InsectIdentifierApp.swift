e9075aa6e7bbdb0ed48bbc61b641b506b5c80d3c
HussnainC <hussnaincodec@gmail.com>
2025-03-11 21:01:35 +0500

    Initial Commit

---
+++ InsectIdentifier/InsectIdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsectIdentifierApp.swift
+//  InsectIdentifier
+//
+//  Created by Hussnain on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct InsectIdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
